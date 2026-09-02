13e336ae730cfb691185768b7596f01c8d68eba4
Ironboy <ironboy@users.noreply.github.com>
2026-09-02 09:37:51 +0200

    example arithmetic operators

---
+++ arithmetic-operators.cs
@@ -0,0 +1,8 @@
+double a = 11;
+double b = 5;
+
+Console.WriteLine($"{a} + {b} = {a + b}");
+Console.WriteLine($"{a} - {b} = {a - b}");
+Console.WriteLine($"{a} * {b} = {a * b}");
+Console.WriteLine($"{a} / {b} = {a / b}");
+Console.WriteLine($"{a} % {b} = {a % b}");
