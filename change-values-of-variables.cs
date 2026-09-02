f328d688e6284ec6b99f1ff6a6bf1cff422cc10c
Ironboy <ironboy@users.noreply.github.com>
2026-09-02 09:53:32 +0200

    chanhing variables with operators

---
+++ change-values-of-variables.cs
@@ -0,0 +1,60 @@
+int counter = 1;
+
+// -------- ADDING ---------------
+
+Console.WriteLine(counter);
+
+// Add 1 to counter - the long way
+counter = counter + 1;
+
+Console.WriteLine(counter);
+
+// Add 1 to counter - the short way
+counter += 1;
+
+Console.WriteLine(counter);
+
+// Add 1 to counter - the shortest way
+// only works when add exactly 1
+counter++;
+
+Console.WriteLine(counter);
+
+// Add 2 to counter - the long way
+counter = counter + 2;
+
+Console.WriteLine(counter);
+
+// Add 2 to counter - the short way
+counter += 2;
+
+Console.WriteLine(counter);
+
+
+// -------- SUBTRACTING ---------------
+
+// Subtract 1 to counter - the long way
+counter = counter - 1;
+
+Console.WriteLine(counter);
+
+// Subtract 1 to counter - the short way
+counter -= 1;
+
+Console.WriteLine(counter);
+
+// Subtract 1 to counter - the shortest way
+// only works when Subtract exactly 1
+counter--;
+
+Console.WriteLine(counter);
+
+// Subtract 2 to counter - the long way
+counter = counter - 2;
+
+Console.WriteLine(counter);
+
+// Subtract 2 to counter - the short way
+counter -= 2;
+
+Console.WriteLine(counter);
